37af6efa84d5954488b06ba4f049e1533aaea558
Remco Westerhoud <remcowesterhoud@msn.com>
2015-09-25 16:47:49 +0200

    Improved receipt generation

--- Clientside/src/Controllers/ReceiptPrinter.java
+++ Clientside/src/Controllers/ReceiptPrinter.java
@@ -28,6 +28,9 @@ public class ReceiptPrinter {
             Product p = entry.getKey();
             System.out.println(p.getName() + " " + entry.getValue() + "x\t$" + df.format(p.getPrice() * entry.getValue()));
         }
+        System.out.println("\nTotal: \t\t$" + df.format(sale.getTotal()));
+        System.out.println("Discount: \t$" + df.format(sale.getDiscount()));
+        System.out.println("New price: \t$" + df.format(sale.getTotal() - sale.getDiscount()));
         System.out.println("------------------------------");
     }
 }
--- Clientside/src/Models/Sale.java
+++ Clientside/src/Models/Sale.java
@@ -8,8 +8,11 @@ import java.util.HashMap;
 public class Sale {
 
     private HashMap<Product, Integer> productList;
+    private double total, discount;
 
     public Sale(){
+        total = 0;
+        discount = 0;
         productList = new HashMap<Product, Integer>();
     }
 
@@ -20,9 +23,18 @@ public class Sale {
         } else {
             productList.put(product, productList.get(product) + 1);
         }
+        total += product.getPrice();
     }
 
     public HashMap<Product, Integer> getProductList() {
         return productList;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public double getDiscount() {
+        return discount;
     }
 }
