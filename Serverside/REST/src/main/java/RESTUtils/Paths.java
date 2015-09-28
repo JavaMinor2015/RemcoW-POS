d115153b04157007244fe2fbbe371dfc83f21ada
Remco Westerhoud <remcowesterhoud@msn.com>
2015-09-28 10:03:56 +0200

    Port changed too 8081

--- Serverside/REST/src/main/java/RESTUtils/Paths.java
+++ Serverside/REST/src/main/java/RESTUtils/Paths.java
@@ -4,7 +4,7 @@ package RESTUtils;
  * Created by Remco on 26-9-2015.
  */
 public class Paths {
-    public static final String BASE_URL = "http://localhost:8080/";
+    public static final String BASE_URL = "http://localhost:8081/";
     public static final String PRODUCT_URI = "products/";
     public static final String DISCOUNT_URI = "discounts/";
 }
--- Serverside/SOAP/src/pos/POSPublisher.java
+++ Serverside/SOAP/src/pos/POSPublisher.java
@@ -7,7 +7,7 @@ import javax.xml.ws.Endpoint;
  */
 public class POSPublisher {
     public static void main(String[] args){
-        final String url = "http://localhost:8080/pos";
+        final String url = "http://localhost:8081/pos";
         System.out.println("Publishing POS at endpoint " + url);
         Endpoint.publish(url + "/world", new HelloWorldImpl());
         Endpoint.publish(url + "/universe", new HelloUniverseImpl());
