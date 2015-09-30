4804b60384e10d49960f8b01890e5221069db4f3
Matthijs van der Meijden <mmjsvandermeijden@gmail.com>
2015-09-30 14:45:58 +0200

    commit

--- Clientside/src/test/java/Utils/HttpRequestTest.java
+++ Clientside/src/test/java/Utils/HttpRequestTest.java
@@ -1,8 +1,11 @@
 package Utils;
 
-import org.junit.Test; 
+import junit.framework.Assert;
+import mockit.Mocked;
+import org.junit.Test;
 import org.junit.Before; 
-import org.junit.After; 
+import org.junit.After;
+
 
 /** 
 * HttpRequest Tester. 
@@ -11,25 +14,32 @@ import org.junit.After;
 * @since <pre>sep 30, 2015</pre> 
 * @version 1.0 
 */ 
-public class HttpRequestTest { 
-
-@Before
-public void before() throws Exception { 
-} 
-
-@After
-public void after() throws Exception { 
-} 
-
-/** 
-* 
-* Method: makeGetReqeust(String url) 
-* 
-*/ 
-@Test
-public void testMakeGetReqeust() throws Exception { 
-//TODO: Test goes here... 
-} 
+public class HttpRequestTest {
 
 
-} 
+    HttpRequest httpRequest;
+
+    String url = "http://localhost:8081/products/22222222";
+
+    @Before
+    public void before() throws Exception {
+        httpRequest = new HttpRequest();
+
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: makeGetReqeust(String url)
+     */
+    @Test
+    public void testMakeGetReqeust() throws Exception {
+        String stringExpected = httpRequest.makeGetReqeust(url);
+        String actual = httpRequest.makeGetReqeust("http://localhost:8081/products/22222222");
+
+        Assert.assertEquals(stringExpected,actual);
+    }
+
+}
